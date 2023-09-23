4a9d2d3005c84bcb973977da20c7138f4b4e6f4e
Shawn Kemp <shawn.kemp@gmail.com>
2023-09-23 12:26:02 -0700

    remove version test

--- helpers.js
+++ helpers.js
@@ -23,6 +23,7 @@ function setquery(p,v){
     history.pushState(null, '', newRelativePathQuery);
 };
 
+//#render and send features to upspire.studio
  function upspirestudio(features) {
         //Add a finished creating preview selector
         var iDiv = document.createElement('div');
@@ -32,7 +33,7 @@ function setquery(p,v){
         if (features == null){features={};}
         var genurl = window.location.href;
         var attr = JSON.stringify(features).replace(/\"/g,"'")
-        var url = 'https://upspirestudio.bubbleapps.io/version-test/api/1.1/wf/update-features';
+        var url = 'https://upspirestudio.bubbleapps.io/api/1.1/wf/update-features';
         var xhr = new XMLHttpRequest();
         xhr.open("POST", url);
         xhr.setRequestHeader("Content-Type", "application/json");
